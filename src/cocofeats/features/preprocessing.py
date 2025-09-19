4554d66fc21410ba1cd437163dd1cf8f718b1aab
yjmantilla <yjmantilla@gmail.com>
2025-09-19 15:41:30 -0400

    Add basic preprocessing and spectrum computation functions for M/EEG data

---
+++ src/cocofeats/features/preprocessing.py
@@ -0,0 +1,109 @@
+from cocofeats.loggers import get_logger
+from cocofeats.utils import get_path,replace_bids_suffix
+from cocofeats.loaders import load_meeg
+from cocofeats.definitions import DatasetConfig
+import glob
+import mne
+import os
+from cocofeats.definitions import PathLike
+
+from cocofeats.definitions import Artifact, FeatureResult
+from typing import Any
+import json
+
+log = get_logger(__name__)
+
+def basic_preprocessing(mne_object, downsample=None, filter_args=None, epoch_config=None, notch_filter=None, extra_artifacts: bool = False) -> FeatureResult:
+
+    if isinstance(mne_object, (str, os.PathLike)):
+        mne_object = load_meeg(mne_object)
+        log.debug("MNEReport: loaded MNE object from file", input=mne_object)
+
+    mne_object = mne_object.copy()
+
+    report = mne.Report(title=f'Basic Preprocessing', verbose='error')
+
+    # EEG before preprocessing:
+    if extra_artifacts:
+        report.add_figure(mne_object.plot(show=False), title=f'Raw Data Before Preprocessing')
+        log.debug("MNEReport: added raw data figure")
+        # psd
+        report.add_figure(mne_object.compute_psd().plot(show=False), title=f'Power Spectral Density Before Preprocessing')
+        log.debug("MNEReport: added raw PSD figure")
+    # Filter the data
+    if notch_filter is not None:
+        mne_object = mne_object.notch_filter(**notch_filter, verbose=False)
+        log.info('NOTCH FILTERED with', notch_filter)
+        if extra_artifacts:
+            report.add_figure(mne_object.compute_psd().plot(show=False), title=f'Power Spectral Density After Notch Filtering')
+            log.debug("MNEReport: added PSD after notch filtering figure")
+
+    if filter_args is not None:
+        mne_object = mne_object.filter(**filter_args, verbose=False)
+        log.info('FILTERED with', filter_args)
+        if extra_artifacts:
+            report.add_figure(mne_object.compute_psd().plot(show=False), title=f'Power Spectral Density After Filtering')
+            log.debug("MNEReport: added PSD after filtering figure")
+
+    # Extract epochs
+    if epoch_config is not None:
+        if isinstance(epoch_config, dict):
+            mne_object = mne.make_fixed_length_epochs(mne_object, preload=True, **epoch_config)
+            log.info('EPOCH SEGMENTATION with', epoch_config)
+        elif isinstance(epoch_config, str):
+            if epoch_config == 'SingleEpoch':
+                mne_object = mne.make_fixed_length_epochs(mne_object, preload=True, duration=mne_object.times[-1], overlap=0)
+                log.info('EPOCH SEGMENTATION with SingleEpoch')
+            elif epoch_config == 'Events':
+                if not hasattr(mne_object, 'annotations') or len(mne_object.annotations) == 0:
+                    raise ValueError("No annotations found in the MNE object for epoching.")
+                events, event_id = mne.events_from_annotations(mne_object)
+                mne_object = mne.Epochs(mne_object, events=events, event_id=event_id, preload=True)
+                log.info('EPOCH SEGMENTATION with Events from annotations')
+            else:
+                raise ValueError(f"Unknown epoch_config: {epoch_config}")
+
+        if extra_artifacts:
+            report.add_figure(mne_object.plot(show=False), title=f'Epochs After Segmentation')
+            log.debug("MNEReport: added epochs after segmentation figure")
+
+
+    if downsample:
+        log.info(f'DOWNSAMPLING TO {downsample}Hz')
+        mne_object = mne_object.resample(downsample, verbose=False)
+        if extra_artifacts:
+            report.add_figure(mne_object.compute_psd().plot(show=False), title=f'Power Spectral Density After Downsampling')
+            log.debug("MNEReport: added PSD after downsampling figure")
+
+    this_metadata = {
+        'downsample': downsample,
+        'filter_args': filter_args,
+        'epoch_config': epoch_config,
+        'notch_filter': notch_filter,
+    }
+
+
+    # Also add metadata to the report
+    if extra_artifacts:
+        report.add_html(
+            f"<pre>{json.dumps(this_metadata, indent=2)}</pre>",
+            title="Metadata",
+            section="Metadata"
+        )
+
+    artifacts ={
+        '.fif': Artifact(
+            item=mne_object,
+            writer=lambda path: mne_object.save(path, overwrite=True)
+        )
+    }
+
+    if extra_artifacts:
+        artifacts['.report.html'] = Artifact(item=report, writer=lambda path: report.save(path, overwrite=True, open_browser=False))
+
+    out = FeatureResult(
+        artifacts=artifacts
+    )
+
+
+    return out
---
+++ src/cocofeats/features/spectral.py
@@ -0,0 +1,108 @@
+from cocofeats.loggers import get_logger
+from cocofeats.utils import get_path,replace_bids_suffix
+from cocofeats.loaders import load_meeg
+from cocofeats.definitions import DatasetConfig
+import glob
+import mne
+import os
+from cocofeats.definitions import PathLike
+from cocofeats.definitions import Artifact, FeatureResult
+from typing import Any
+import xarray as xr
+import json
+
+log = get_logger(__name__)
+
+def spectrum(
+    meeg: mne.io.BaseRaw | mne.BaseEpochs,
+    compute_psd_kwargs: dict[str, Any] | None = None,
+    extra_artifacts: bool = True) -> FeatureResult:
+    """
+    Compute the power spectral density of M/EEG data.
+
+    Parameters
+    ----------
+    meeg : mne.io.BaseRaw or mne.BaseEpochs
+        The M/EEG data to analyze. Can be raw data or epochs.
+    compute_psd_kwargs : dict, optional
+        Additional keyword arguments to pass to `mne.compute_psd`.
+    extra_artifacts : bool, optional
+        Whether to generate extra artifacts (MNE Report). Default is True.
+    Returns
+    -------
+    dict
+        A dictionary containing the power spectral density results, metadata, and artifacts (MNE Report).
+    """
+
+
+    if isinstance(meeg, (str, os.PathLike)):
+        meeg = load_meeg(meeg)
+        log.debug("MNEReport: loaded MNE object from file", input=meeg)
+
+    if compute_psd_kwargs is None:
+        compute_psd_kwargs = {}
+
+    if 'fmax' in compute_psd_kwargs:
+        if compute_psd_kwargs['fmax'] is not None and compute_psd_kwargs['fmax'] > meeg.info['sfreq'] / 2:
+            log.warning("fmax is greater than Nyquist frequency, adjusting to Nyquist")
+            compute_psd_kwargs['fmax'] = meeg.info['sfreq'] / 2
+
+    spectra = meeg.compute_psd(**compute_psd_kwargs)
+    log.debug("MNEReport: computed spectra", spectra=spectra)
+
+    if extra_artifacts:
+        report = mne.Report(title=f'Spectrum', verbose='error')
+        report.add_figure(spectra.plot(show=False), title=f'Spectrum')
+        log.debug("MNEReport: computed report")
+
+    extra_artifact = Artifact(item=report, writer=lambda path: report.save(path, overwrite=True, open_browser=False))
+    if isinstance(meeg, mne.io.BaseRaw):
+        this_xarray = xr.DataArray(
+            data=spectra.get_data(picks='eeg'),
+            dims=['spaces', 'frequencies'],
+            coords={
+                'spaces': spectra.ch_names,
+                'frequencies': spectra.freqs
+            },
+        )
+    elif isinstance(meeg, mne.BaseEpochs):
+        this_xarray = xr.DataArray(
+            data=spectra.get_data(picks='eeg'),
+            dims=['epochs', 'spaces', 'frequencies'],
+            coords={
+                'epochs': list(range(len(spectra))),
+                'spaces': spectra.ch_names,
+                'frequencies': spectra.freqs,
+            },
+        )
+
+
+    this_metadata = {
+        'compute_psd_kwargs': compute_psd_kwargs,
+    }
+
+    this_xarray.attrs['metadata'] = json.dumps(this_metadata, indent=2)
+
+    # Also add metadata to the report
+    if extra_artifacts:
+        extra_artifact.item.add_html(
+            f"<pre>{json.dumps(this_metadata, indent=2)}</pre>",
+            title="Metadata",
+            section="Metadata"
+        )
+
+    artifacts ={
+        '.nc': Artifact(
+            item=this_xarray,
+            writer=lambda path: this_xarray.to_netcdf(path)
+        )
+    }
+
+    if extra_artifacts:
+        artifacts['.report.html'] = extra_artifact
+
+    out = FeatureResult(
+        artifacts=artifacts
+    )
+    return out
+
